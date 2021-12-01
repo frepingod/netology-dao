60650c8dc7b971246a327d00a2347e90e47f96fc
frepingod <frepingod@gmail.com>
2021-12-01 20:30:40 +0300

    fix liquibase migration, now it's work

--- src/main/java/ru/netology/repository/DaoRepository.java
+++ src/main/java/ru/netology/repository/DaoRepository.java
@@ -1,12 +1,9 @@
 package ru.netology.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.BufferedReader;
 import java.io.IOException;
